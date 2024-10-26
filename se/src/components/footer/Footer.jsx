4bb295e231912d45d917bd065b7f070d7b803790
Ihar Skavysh <igogoblin@gmail.com>
2024-10-26 21:03:48 +0300

    feat: add images for theme

--- se/src/components/footer/Footer.jsx
+++ se/src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
 import "./style.css";
 import esGray from "../../assets/images/esGray.svg";
 import copyright from "../../assets/images/copyright.svg";
+import sunD from "../../assets/images/sunDark.svg";
+import moonL from "../../assets/images/moonLight.svg";
+// import sunL from "../../assets/images/sunLight.svg";
+// import moonD from "../../assets/images/moonDark.svg";
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__language">en ru</div>
-        <div className="footer__dark-mode">l d</div>
+        <div className="footer__dark-mode">
+          <img src={sunD} alt="light theme" />
+          <img src={moonL} alt="dark theme" />
+        </div>
         <div className="footer__content">
           <img src={esGray} className="footer__img" alt="logo author" />
           <img src={copyright} className="footer__img" alt="copyright" />
