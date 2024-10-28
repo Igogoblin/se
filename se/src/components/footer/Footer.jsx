7b8c585cc2570c057f21ad0f8a616f2d8e2712ce
Ihar Skavysh <igogoblin@gmail.com>
2024-10-28 15:56:34 +0300

    feat: add redux toolkit

--- se/src/components/footer/Footer.jsx
+++ se/src/components/footer/Footer.jsx
@@ -1,19 +1,39 @@
 import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setTheme } from "../../store/themeSlice";
 import esGray from "../../assets/images/esGray.svg";
 import copyright from "../../assets/images/copyright.svg";
 import sunD from "../../assets/images/sunDark.svg";
 import moonL from "../../assets/images/moonLight.svg";
-// import sunL from "../../assets/images/sunLight.svg";
-// import moonD from "../../assets/images/moonDark.svg";
+import sunL from "../../assets/images/sunLight.svg";
+import moonD from "../../assets/images/moonDark.svg";
 const Footer = () => {
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.theme);
+  const setLight = () => {
+    dispatch(setTheme("light"));
+  };
+  const setDark = () => {
+    dispatch(setTheme("dark"));
+  };
   return (
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__buttons">
           <div className="footer__language">En Ru</div>
           <div className="footer__dark-mode">
-            <img src={sunD} alt="light theme" />
-            <img src={moonL} alt="dark theme" />
+            <img
+              onClick={setLight}
+              src={theme === "light" ? sunD : sunL}
+              alt="light theme"
+              className={theme === "dark" ? "footer__active" : ""}
+            />
+            <img
+              onClick={setDark}
+              src={theme === "dark" ? moonD : moonL}
+              alt="dark theme"
+              className={theme === "light" ? "footer__active" : ""}
+            />
           </div>
         </div>
 
